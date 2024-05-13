8e032ee83d3b85041dc4989b6ef89ace20b0edaf
seoyerin <seyerin12@naver.com>
2024-05-14 06:23:43 +0900

    설정 기능 구현

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,18 +1,32 @@
 import styled from "styled-components";
 import Header from "../components/common/Header";
 import MainFrame from "../components/common/MainFrame";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MainProvider from "../provider/MainProvider";
 
 export default function Setting() {
+  const USERNAME = "userName";
   const [level, setLevel] = useState("1");
-  const [userName, setUserName] = useState("얼렁뚱땅얼렁뚱땅라라");
+  const [userName, setUserName] = useState(localStorage.getItem(USERNAME) ? localStorage.getItem(USERNAME) : "");
+
+  async function setFetch() {
+    try {
+      const response = await fetch("json/set.json");
+      const data = await response.json();
+      setLevel(Math.floor(data.count / 30));
+    } catch (err) {}
+  }
+
+  useEffect(() => {
+    setFetch();
+    console.log(level);
+  }, []);
   return (
     <MainProvider>
       <Header show={"true"} anime={"setting"}>
         <SettingTool>
-          <UserInpo>
-            <UserLevel>{`LV ${level}`}</UserLevel>
+          <UserInpo $nameLength={userName.length}>
+            <UserLevel>{`Lv ${level}`}</UserLevel>
             <span>{`\u00a0\u00a0${userName}`}</span>
           </UserInpo>
           <span>로그아웃</span>
@@ -30,10 +44,26 @@ export default function Setting() {
 }
 
 const Img = styled.img`
-  width: 40vw;
-  position: absolute;
-  top: 70px;
-  right: 0;
+  width: 42vw;
+  position: relative;
+  top: 3vh;
+  left: 10vw;
+  animation-name: move;
+  animation-duration: 1s;
+  animation-iteration-count: 1;
+
+  @keyframes move {
+    0% {
+      width: 35vw;
+      top: 5.2vh;
+      left: 0;
+    }
+    100% {
+      width: 42vw;
+      top: 3vh;
+      left: 10vw;
+    }
+  }
 `;
 
 const SettingTool = styled.section`
@@ -46,8 +76,8 @@ const SettingTool = styled.section`
   align-items: center;
   animation-name: showText;
   animation-iteration-count: 1;
-  animation-duration: 1s;
-  transition-duration: 1s;
+  animation-duration: 0.6s;
+  transition-duration: 0.6s;
 
   @keyframes showText {
     0% {
@@ -65,16 +95,20 @@ const SettingTool = styled.section`
 `;
 
 const UserInpo = styled.section`
-  width: 85%;
+  /* width: 50%; */
+  /* width: 200px; */
+  width: ${(props) => (props.$nameLength > 0 ? `${125 + props.$nameLength * 25.95}px` : "125px")};
   height: auto;
   font-size: 30px;
   display: flex;
   justify-content: center;
+  align-items: center;
   position: relative;
   /* background-color: aqua; */
 `;
 
 const UserLevel = styled.span`
+  font-size: 25px;
   position: absolute;
   left: 0;
 `;

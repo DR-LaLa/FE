c41d146b35967c4b7c951632edae252acdd5004b
seoyerin <seyerin12@naver.com>
2024-06-01 19:09:10 +0900

    로그인 코드 수정]

--- src/components/common/FormFrame.jsx
+++ src/components/common/FormFrame.jsx
@@ -27,8 +27,10 @@ export default function FormFrame(props) {
     keys.forEach((x) => {
       if (x != "count" && x != "level") {
         if (tmp[x] !== "") {
-          if (duplicationState && duplication && passwordCheck == "true") {
-            setDisabled("true");
+          if (duplicationState && duplication) {
+            if (passwordCheck == "true" || passwordCheck == "none") {
+              setDisabled("true");
+            }
           } else {
             setDisabled("false");
           }

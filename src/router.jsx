61d08c7920a056049a86ff228cd7f692b87624fe
seoyerin <seyerin12@naver.com>
2024-07-30 23:58:05 +0900

    기타 코드 수정

--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import SignUp from "./pages/Signup";
+import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
 import Setting from "./pages/Setting";
